4403b044d1ba2d639986d1c83f80c790060bf993
Mo Alwan <126471585+ebdacomp@users.noreply.github.com>
2023-03-10 19:27:45 +0300

    Update Task6.php

--- src/Task6.php
+++ src/Task6.php
@@ -1,29 +1,21 @@
 <?php
-// Task 6 - Create a function that returns a list of users by their name
 function searchUsersByName($database, $name)
 {
-     // Task 6.1 edit the query below to return a list of users by their name
-     $query = "";
-
-     // don't toach following line and don't worry about this line, it just makes the query easier to read
+     // Query the database for user data
+     $query = "SELECT * FROM users WHERE name LIKE '%$name%' ORDER BY id DESC";
      $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
+     $result = $database->query($query);
 
+     // Check for query errors
+     if (!$result) {
+          die('Query Error (' . $database->errno . ') ' . $database->error);
+     }
 
-     // Task 6.2 complete the function body to return the users
-     // hint : use $database->query($query) to execute the query
-     // hint: use fetch_assoc to get the result row
+     // Loop through the results and store them in an array
+     $users = [];
+     while ($row = $result->fetch_assoc()) {
+          $users[] = $row;
+     }
 
+     return $users;
 }
-// example output of searchUsersByName($database, 'John') 2 rows
-// [
-//     [
-//         'id' => 1,
-//         'name' => 'John Doe',
-//         'email' => 'john@example.com'
-//     ],
-//     [
-//         'id' => 2,
-//         'name' => 'John Smith',
-//         'email' => 'john@example.com'
-//     ]
-// ]
