903e4d80a190b033d48c48c9019ce200de9613c7
Mo Alwan <126471585+ebdacomp@users.noreply.github.com>
2023-03-10 19:25:52 +0300

    update task 2

--- src/Task2.php
+++ src/Task2.php
@@ -1,21 +1,19 @@
 <?php
-// Task 2 - Create a function that returns a user by their ID
 function getUserById($database, $userId)
 {
     // Prepare the query and bind the user ID parameter
-    // Task 2.1 edit the query below to return a user by their ID
-    $query = "..... id = ?";
-    // don't toach following line and don't worry about this line, it just makes the query easier to read
+    $query = "SELECT * FROM users WHERE id = ?";
     $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
+    $stmt = $database->prepare($query);
+    $stmt->bind_param("i", $userId);
 
-    // Task 1.2 complete the function body to return the users
-    // hint: use fetch_assoc to get the result row
+    // Execute the query and get the result
+    $result = $stmt->execute();
+    if (!$result) {
+        die('Query Error (' . $stmt->errno . ') ' . $stmt->error);
+    }
 
+    // Fetch the result row and return it
+    $row = $stmt->get_result()->fetch_assoc();
+    return $row ? $row : null;
 }
-
-//example output of getUsers($database) 1 row
-// [
-//     'id' => 1,
-//     'name' => 'John Doe',
-//     'email' => 'john@example.com'
-// ]
