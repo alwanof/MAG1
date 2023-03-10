e9115a2b7ebaca2d1914145ae43f1bdf01cfcb29
Mo Alwan <126471585+ebdacomp@users.noreply.github.com>
2023-03-10 19:27:16 +0300

    update task 5

--- src/Task5.php
+++ src/Task5.php
@@ -1,18 +1,18 @@
 <?php
-// Task 5 - Create a function that removes a user from the database
+
 function removeUser($database, $id)
 {
 
-    // Task 5.1 edit the query below to remove a user from the database
-    $sql = "";
 
-    // don't toach following line and don't worry about this line, it just makes the query easier to read
+    $sql = "DELETE FROM users WHERE id='$id'";
+    // requlare expression to remove all spaces in my sql query
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
-    // Task 5.2 complete the function body to remove the user
-    // hint: use $database->query($sql) to execute the query
+    if ($database->query($sql) === TRUE) {
+        return "Record removed successfully";
+    }
 
+    return false;
+
+    $database->close();
 }
-// example output :
-// 'Record removed successfully' if the user was removed successfully
-// false if the user was not removed successfully
