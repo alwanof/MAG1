4e866a9e880368e5ae5a01c2b60258e1a9277123
Mo Alwan <126471585+ebdacomp@users.noreply.github.com>
2023-03-10 19:26:46 +0300

    update task 4

--- src/Task4.php
+++ src/Task4.php
@@ -1,20 +1,20 @@
 <?php
-// Task 4 - Create a function that updates a user in the database
+
 function updateUser($database, $data, $id)
 {
 
     $name = $data['name'];
     $email = $data['email'];
-    // Task 4.1 edit the query below to update a user in the database
-    $sql = "";
 
-    // don't toach following line and don't worry about this line, it just makes the query easier to read
+    $sql = "UPDATE users SET name= '$name', email ='$email' WHERE id='$id'";
+    // requlare expression to remove all spaces in my sql query
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
-    // Task 4.2 complete the function body to update the user
-    // hint: use $database->query($sql) to execute the query
+    if ($database->query($sql) === TRUE) {
+        return "Record updated successfully";
+    }
 
+    return false;
+
+    $database->close();
 }
-// example output :
-// 'Record updated successfully' if the user was updated successfully
-// false if the user was not updated successfully
