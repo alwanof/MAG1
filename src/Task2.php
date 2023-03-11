de2a6f1c3bc153d2b51b56fefdcd75b3b00d0817
AhmedOmar11122 <94564667+AhmedOmar11122@users.noreply.github.com>
2023-03-11 18:30:09 +0300

    Update Task2.php
    task2

--- src/Task2.php
+++ src/Task2.php
@@ -4,7 +4,7 @@ function getUserById($database, $userId)
 {
     // Prepare the query and bind the user ID parameter
     // Task 2.1 edit the query below to return a user by their ID
-    $query = "..... id = ?";
+    $query = "SELECT * FROM users WHERE id =". $userId;
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
 
