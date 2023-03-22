ba173a1a62928a9a437d21a4b451c907ee7ef305
AhmedOmar11122 <94564667+AhmedOmar11122@users.noreply.github.com>
2023-03-23 00:00:58 +0300

    Update Task2.php

--- src/Task2.php
+++ src/Task2.php
@@ -5,6 +5,14 @@ function getUserById($database, $userId)
     // Prepare the query and bind the user ID parameter
     // Task 2.1 edit the query below to return a user by their ID
     $query = "SELECT * FROM users WHERE id =". $userId;
+    $stmt = $database->prepare($sql);
+    $stmt->bindParam(':userId', $userId);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $user ? $user : null;
+    
+    
+    
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
 
