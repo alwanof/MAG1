3f46a7f2457d755c5f3dc59a12434c85d8a6896e
a.YILDIRIM <100519286+Apo-12@users.noreply.github.com>
2023-03-26 14:45:33 +0300

    solution 2

--- src/Task2.php
+++ src/Task2.php
@@ -4,12 +4,22 @@ function getUserById($database, $userId)
 {
     // Prepare the query and bind the user ID parameter
     // Task 2.1 edit the query below to return a user by their ID
-    $query = "..... id = ?";
+    $query = "SELECT *FROM users WHERE id = ?";
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
 
     // Task 1.2 complete the function body to return the users
     // hint: use fetch_assoc to get the result row
+    if ($database->connect_error) {
+  die("Connection failed: " . $database->connect_error);
+}
+
+$userId = "SELECT *FROM users WHERE id = ?";
+$result = $database->query($userId);
+    
+    while($row = $result->fetch_assoc()) {
+    echo . $row["id"]."<br>" ;
+  }
 
 }
 
