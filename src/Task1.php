5fa7c4785fac14e38d88a7b3fba7bfbb1ca1a7c5
NAFIE ASFOUR <37361982+nafieasfour@users.noreply.github.com>
2023-03-27 13:09:56 +0300

    Nafie_Asfour_Task1

--- src/Task1.php
+++ src/Task1.php
@@ -4,13 +4,22 @@ function getUsers($database)
 {
      // Query the database for user data
      // Task1.1 edit the query below to return all users from the database
-     $query = "";
+     $query = "SELECT * FROM users";
 
      // don't toach following line and don't worry about this line, it just makes the query easier to read
      $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
 
      // Task 1.2 complete the function body to return the users
      // hint: use fetch_assoc to get the result row
+     $result = $database ->query($query);
+     if (!result) {
+          die('Query Error (' .$database->errno .') ' . $database->error);
+     }
+     $users = [];
+     while($row = $result->fetchassoc()) {
+          $users=$row;
+     }
+     return $users;
 }
 
 // example output of getUsers($database) 2 rows
