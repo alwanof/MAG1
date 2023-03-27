f28e2412969837d9391185f6d9465066746c6e21
NAFIE ASFOUR <37361982+nafieasfour@users.noreply.github.com>
2023-03-27 14:07:25 +0300

    Nafie_Asfour_Task4

--- src/Task4.php
+++ src/Task4.php
@@ -6,13 +6,20 @@ function updateUser($database, $data, $id)
     $name = $data['name'];
     $email = $data['email'];
     // Task 4.1 edit the query below to update a user in the database
-    $sql = "";
+    $sql = "UPDATE users SET name = "updated_name", email = "updated_new_email@gmail.com" WHERE name = "name" and email ="email"";
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
     // Task 4.2 complete the function body to update the user
     // hint: use $database->query($sql) to execute the query
+    $result = $database ->query($query);
+     if (result[]) {
+          die('false (' .$database->errno .') ' . $database->error);
+     } else {
+        return "Record updated successfully";
+     }
+}
 
 }
 // example output :
