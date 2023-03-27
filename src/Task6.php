1454dc1b2837cd2fb640b03357dc213f9d7b4803
AhmedOmar11122 <94564667+AhmedOmar11122@users.noreply.github.com>
2023-03-27 21:13:12 +0300

    Update Task6.php

--- src/Task6.php
+++ src/Task6.php
@@ -3,7 +3,7 @@
 function searchUsersByName($database, $name)
 {
      // Task 6.1 edit the query below to return a list of users by their name
-     $query = "";
+     $query = "SELECT * FROM users WHERE name LIKE '%$name%' ORDER BY id DESC";
 
      // don't toach following line and don't worry about this line, it just makes the query easier to read
      $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
@@ -12,7 +12,11 @@ function searchUsersByName($database, $name)
      // Task 6.2 complete the function body to return the users
      // hint : use $database->query($query) to execute the query
      // hint: use fetch_assoc to get the result row
-
+$result = $database->query($query);
+	 $rows = [];
+	 while($row = $result->fetch_assoc())
+		 $rows[] = $row;
+	 return $rows;
 }
 // example output of searchUsersByName($database, 'John') 2 rows
 // [
