ae3e50afbedbd2f868382274b0b359c45629434a
NAFIE ASFOUR <37361982+nafieasfour@users.noreply.github.com>
2023-03-27 14:12:06 +0300

    Nafie_Asfour_Task5

--- src/Task5.php
+++ src/Task5.php
@@ -4,14 +4,19 @@ function removeUser($database, $id)
 {
 
     // Task 5.1 edit the query below to remove a user from the database
-    $sql = "";
+    $sql = "DELETE FROM users WHERE name = "name"";
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
     // Task 5.2 complete the function body to remove the user
     // hint: use $database->query($sql) to execute the query
-
+     $result = $database ->query($query);
+     if (result[]) {
+          die('false (' .$database->errno .') ' . $database->error);
+     } else {
+        return "Record removed successfully";
+     }
 }
 // example output :
 // 'Record removed successfully' if the user was removed successfully
