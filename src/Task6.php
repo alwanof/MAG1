b8b68b34a92110ece8a872f497edb6df15ab95f4
Bashir Al-Rayes <85109853+bashir-alrayes@users.noreply.github.com>
2023-03-29 00:22:31 +0300

    BashirAlrayesTask6

--- src/Task6.php
+++ src/Task6.php
@@ -3,7 +3,8 @@
 function searchUsersByName($database, $name)
 {
      // Task 6.1 edit the query below to return a list of users by their name
-     $query = "";
+    
+     $query = "SELECT * FROM users WHERE name LIKE '%Bashir%' ORDER BY name DESC";
 
      // don't toach following line and don't worry about this line, it just makes the query easier to read
      $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
@@ -12,6 +13,11 @@ function searchUsersByName($database, $name)
      // Task 6.2 complete the function body to return the users
      // hint : use $database->query($query) to execute the query
      // hint: use fetch_assoc to get the result row
+     $users = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $users[] = $row;
+    }
+      return $users;
 
 }
 // example output of searchUsersByName($database, 'John') 2 rows
