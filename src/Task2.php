6a651db595f718a754f60c9d0a4901a42256e9bc
Bashir Al-Rayes <85109853+bashir-alrayes@users.noreply.github.com>
2023-03-29 00:15:53 +0300

    BashirAlrayesTask2

--- src/Task2.php
+++ src/Task2.php
@@ -4,12 +4,19 @@ function getUserById($database, $userId)
 {
     // Prepare the query and bind the user ID parameter
     // Task 2.1 edit the query below to return a user by their ID
-    $query = "..... id = ?";
+    
+      $query = "SELECT * FROM users WHERE id = 1";
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
 
     // Task 1.2 complete the function body to return the users
     // hint: use fetch_assoc to get the result row
+    
+     $users = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $users[] = $row;
+        }
+    return $users;
 
 }
 
