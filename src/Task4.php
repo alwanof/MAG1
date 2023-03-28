36d129f8a933bd99a1d705f87d4a9ffff5927bd3
Bashir Al-Rayes <85109853+bashir-alrayes@users.noreply.github.com>
2023-03-28 16:01:55 +0300

    BashirAlrayesTask4

--- src/Task4.php
+++ src/Task4.php
@@ -6,13 +6,24 @@ function updateUser($database, $data, $id)
     $name = $data['name'];
     $email = $data['email'];
     // Task 4.1 edit the query below to update a user in the database
-    $sql = "";
+    $sql = UBDATE users
+        SET email 'bashir.alrayes1@gmail.com',
+        name 'Bashir alrayes'
+            where id=1;
+    
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
     // Task 4.2 complete the function body to update the user
     // hint: use $database->query($sql) to execute the query
+    
+    if ($database->query($sql)) {
+     echo 'New record ubdated successfully';
+  } else {
+    echo 'New record not ubdated successfully';
+         
+  }
 
 }
 // example output :
