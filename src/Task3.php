edf853a0ee028df20458546d2f781401b8c4081b
Bashir Al-Rayes <85109853+bashir-alrayes@users.noreply.github.com>
2023-03-28 15:37:26 +0300

    BashirAlrayesTask3

--- src/Task3.php
+++ src/Task3.php
@@ -8,12 +8,25 @@ function insertUser($database, $data)
 
     // Task 3.1 edit the query below to insert a new user into the database
     $sql = "";
+    $sql = "INSERT INTO users (name, email)
+     VALUES ('Bashir', 'Alrayes', 'bashir.freedom@gmail.com')";
+    
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
     // Task 3.2 complete the function body to insert the user
     // hint: use $database->query($sql) to execute the query
+   
+    
+     if ($database->query($sql)) {
+     echo 'New record created successfully';
+  } else {
+    echo 'New record not created successfully';
+         
+  }
+    
+    
 }
 
 // example output :
