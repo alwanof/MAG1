097893a5bb97ecae90b3bdca2bf87cb170f79527
a.YILDIRIM <100519286+Apo-12@users.noreply.github.com>
2023-03-29 22:06:11 +0300

    update soltion 4

--- src/Task4.php
+++ src/Task4.php
@@ -6,19 +6,18 @@ function updateUser($database, $data, $id)
     $name = $data['name'];
     $email = $data['email'];
     // Task 4.1 edit the query below to update a user in the database
-    $sql = "UPDATE 'users' SET 'name'='ahmedmohamed' WHERE 'users'.'id'=2";
+    $sql = "UPDATE users SET name = '$name', email = '$email' WHERE id = '$id'";
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
     $sql = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $sql);
 
     // Task 4.2 complete the function body to update the user
     // hint: use $database->query($sql) to execute the query
-if ($database->query($sql)){
-    echo "Record updated successfully";
-} else {
-    echo "false" . $database->error;
-}
-    
+$result = $database->query($sql);
+	if($result)
+		return 'Record updated successfully';
+	else
+		return false;
 }
 // example output :
 // 'Record updated successfully' if the user was updated successfully
