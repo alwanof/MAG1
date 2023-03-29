e2e5e2c29c7d354dda9d23409e4991789fd3c51e
a.YILDIRIM <100519286+Apo-12@users.noreply.github.com>
2023-03-29 22:03:37 +0300

    updata solution3

--- src/Task3.php
+++ src/Task3.php
@@ -7,7 +7,7 @@ function insertUser($database, $data)
     $email = $data['email'];
 
     // Task 3.1 edit the query below to insert a new user into the database
-    $sql = "INSERT INTO users ('name','email') VALUES ('ahmed',ahmed@gmail.com)";
+    $sql = "INSERT INTO users ('name','email') VALUES ('$name','$email')";
     
 
     // don't toach following line and don't worry about this line, it just makes the query easier to read
@@ -15,11 +15,12 @@ function insertUser($database, $data)
 
     // Task 3.2 complete the function body to insert the user
     // hint: use $database->query($sql) to execute the query
-    if ($database->query($sql){
-        echo "New record created successfully";
-    }else {
-        echo "false" .$database->error;
-    }
+  $result = $database->query($sql);
+	if($result)
+		return 'New record created successfully';
+	else
+		return false; 
+     
 }
 
 // example output :
