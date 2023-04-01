b95999c742e9730e04723e1f1a387296f1d8144b
Sa19s2 <126162120+Sa19s2@users.noreply.github.com>
2023-04-01 05:40:41 +0300

    Update Task6.php

--- src/Task6.php
+++ src/Task6.php
@@ -3,7 +3,8 @@
 function searchUsersByName($database, $name)
 {
      // Task 6.1 edit the query below to return a list of users by their name
-     $query = "";
+     $query = "SELECT * FROM users
+               WHERE name LIKE 'John' ";
 
      // don't toach following line and don't worry about this line, it just makes the query easier to read
      $query = preg_replace(array('/\s*,\s*/', '/\s*=\s*/'), array(',', '='), $query);
